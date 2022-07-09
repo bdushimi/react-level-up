88ebf0ec18e33e77e67fd390de3ab4d554fe2136
bdushimi <lespharaons@hotmail.com>
2022-07-09 23:52:13 +0200

    Project files setup

--- project/src/App.js
+++ project/src/App.js
@@ -1,22 +1,12 @@
 import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
-import Counter from './Counter';
+import items from './data';
+import Menu from './Menu';
+import Categories from './Categories';
 function App() {
 
   const [questions, setQuestions] = useState(data)
   return <main>
-    <div class="container">
-      {/* <h3>Questions and answers about login</h3>
-      <section class="info">
-        {
-          questions.map((question) => {
-            return <SingleQuestion key={question.id} {...question} />
-          })
-        }
-      </section> */}
-      <Counter />
-    </div>
+   <h2>menu project setup</h2>;
   </main>
 }
 

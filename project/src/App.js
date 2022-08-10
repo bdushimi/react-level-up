c87d5b4afd6420967bce40dfad6db21692d9051a
bdushimi <lespharaons@hotmail.com>
2022-08-10 07:00:33 +0200

    adds dynamic menu filtering

--- project/src/App.js
+++ project/src/App.js
@@ -2,11 +2,34 @@ import React, { useState } from 'react';
 import items from './data';
 import Menu from './Menu';
 import Categories from './Categories';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
 
-  const [questions, setQuestions] = useState(data)
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const filteredItems = items.filter(item => item.category === category);
+    setMenuItems(filteredItems);
+  }
+
   return <main>
-   <h2>menu project setup</h2>;
+    <section class="menu section">
+      <div class="title">
+        <h2>Our Menu</h2>
+        <div class="underline"></div>
+      </div>
+    </section>
+    <Categories categories={categories} filterItems={filterItems} />
+    <Menu items={ menuItems} />
   </main>
 }
 

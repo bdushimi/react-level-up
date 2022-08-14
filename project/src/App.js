0b1f5b24e510ea6e83169c9703c50c112638d889
bdushimi <lespharaons@hotmail.com>
2022-08-14 11:49:10 +0200

    Generates n of paragraphs

--- project/src/App.js
+++ project/src/App.js
@@ -7,15 +7,34 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log("Form Submitted!")
- }
+    let nOfParagraphs = parseInt(count)
+    if (nOfParagraphs < 0) nOfParagraphs = 1
+    if (nOfParagraphs > 8) nOfParagraphs = 8
+
+    setText(data.slice(0, nOfParagraphs))
+  }
 
   return (
     <section className="section-center">
       <h3>Generate lorem ipsum paragraphs</h3>
-      <form className="lorem-form" onSubmit={ handleSubmit}>
-
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs : </label>
+        <input
+          type="number"
+          name="amount"
+          id="amount" value={count}
+          onChange={(e) => setCount(e.target.value)} />
+        <button className="btn" type="submut">Generate</button>
       </form>
+      <article className="lorem-text">
+        {
+          text.map((item, index) => {
+            return (
+              <p id={index}>{ item}</p>
+            )
+          })
+         }
+      </article>
     </section>
   )
 }

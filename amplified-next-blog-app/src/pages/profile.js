c3522414ad0617252c19b9571ffa6467aa1d6a75
bdushimi <lespharaons@hotmail.com>
2023-05-01 20:18:16 +0200

    Changes amplify default colors to custom ones

--- amplified-next-blog-app/src/pages/profile.js
+++ amplified-next-blog-app/src/pages/profile.js
@@ -20,8 +20,8 @@ const Profile = () => {
     return (
         <div>
             <h1 className="text-3xl font-semibold tracking-wide mt-6">Profile</h1>
-            <h1 className="font-medium text-gray-500 my-2">{user.username}</h1>
-            <p className="text-sm text-gray-500 mb-6">{user.attributes.email}</p>
+            <h1 className="font-medium text-gray-500 my-2">Username : {user.username}</h1>
+            <p className="text-sm text-gray-500 mb-6">Email: {user.attributes.email}</p>
             <AmplifySignOut />
         </div>
     )

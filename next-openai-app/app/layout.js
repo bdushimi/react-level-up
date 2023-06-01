a36a17359dda0c972d593860e954f14870f8b32b
bdushimi <lespharaons@hotmail.com>
2023-06-01 14:26:30 +0200

    Adds Auth feature

--- next-openai-app/app/layout.js
+++ next-openai-app/app/layout.js
@@ -1,4 +1,6 @@
 import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
   title: 'PromptAI',
@@ -9,12 +11,16 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
       <body>
+      <Provider>
         <div className='main'>
           <div className='gradient' />
         </div>
+
         <main className='app'>
+          <Nav />
           {children}
         </main>
+      </Provider>
       </body>
     </html>
   )
